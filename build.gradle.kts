97dc2890afbc8c57b143517bb58cadfa3445fc30
sbritocorral <s.britocorral@gmail.com>
2025-01-03 11:53:28 +0100

    chore: add ktlint configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
     id("org.jetbrains.kotlinx.kover") version "0.8.3"
 }
 
