3738e1d05baa053a1c690290797b6e23af5ae1e3
sbritocorral <s.britocorral@gmail.com>
2025-01-03 11:50:54 +0100

    test: add application bootstrap test for coverage

--- src/test/kotlin/com/codefactory/urlshortener/UrlShortenerApplicationTests.kt
+++ src/test/kotlin/com/codefactory/urlshortener/UrlShortenerApplicationTests.kt
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Import
 
 @Import(TestcontainersConfiguration::class)
 @SpringBootTest
-class UrlshortenerApplicationTests {
+class UrlShortenerApplicationTests {
 
     @Test
-    fun contextLoads() {
+    fun `application main should start without throwing exceptions`() {
+        main(arrayOf())
     }
-
 }
